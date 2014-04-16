415c9293a29595ecbfa0f957fb86ae8d86f433e8
Moritz Horn <moritz.horn@gmail.com>
2014-04-16 09:52:02 +0200

    arrayed api&input

--- demo/app.js
+++ demo/app.js
@@ -1,16 +1,13 @@
 angular.module('myApp', ['ui.bootstrap','ngSuggest']);
 
 function myController($scope) {
-    $scope.input1 = "";
-    $scope.input2 = "";
-    $scope.input3 = "";
-    $scope.input4 = "";
-    $scope.input5 = "";
-    $scope.api1 = "http://ws.gbv.de/suggest/geonames/?country=DE&count=20&searchterm=";
-    $scope.api2 = "http://ws.gbv.de/suggest/gn250/?count=20&searchterm=";
-    $scope.api3 = "http://ws.gbv.de/suggest/gnd/?count=20&type=&searchterm=";
-    $scope.api4 = "http://ws.gbv.de/suggest/iconclass/?type=all&count=20&searchterm=";
-    $scope.api5 = "https://de.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&search=";
+    $scope.input = ["","","","",""];
+    $scope.api = [
+    "http://ws.gbv.de/suggest/geonames/?country=DE&count=20&searchterm=",
+		"http://ws.gbv.de/suggest/gn250/?count=20&searchterm=",
+		"http://ws.gbv.de/suggest/gnd/?count=20&type=&searchterm=",
+		"https://de.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&search="
+		];
 		$scope.onSelect1 = function ($item, $model, $label) {
 			$scope.$item1 = $item;
 			$scope.$model1 = $model;
@@ -29,10 +26,10 @@ function myController($scope) {
 			$scope.$label3 = $label;
 			console.log($item)
 		};
-		$scope.onSelect5 = function ($item, $model, $label) {
-			$scope.$item5 = $item;
-			$scope.$model5 = $model;
-			$scope.$label5 = $label;
+		$scope.onSelect4 = function ($item, $model, $label) {
+			$scope.$item4 = $item;
+			$scope.$model4 = $model;
+			$scope.$label4 = $label;
 			console.log($item)
 		};
 }
