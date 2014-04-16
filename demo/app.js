9aacb6d3b55cd6d2c8cde49aa4a12c97c375718e
Moritz Horn <moritz.horn@gmail.com>
2014-04-16 08:57:10 +0200

    implemented onSelect

--- demo/app.js
+++ demo/app.js
@@ -11,4 +11,28 @@ function myController($scope) {
     $scope.api3 = "http://ws.gbv.de/suggest/gnd/?count=20&type=&searchterm=";
     $scope.api4 = "http://ws.gbv.de/suggest/iconclass/?type=all&count=20&searchterm=";
     $scope.api5 = "https://de.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&search=";
+		$scope.onSelect1 = function ($item, $model, $label) {
+			$scope.$item1 = $item;
+			$scope.$model1 = $model;
+			$scope.$label1 = $label;
+			console.log($item)
+		};
+		$scope.onSelect2 = function ($item, $model, $label) {
+			$scope.$item2 = $item;
+			$scope.$model2 = $model;
+			$scope.$label2 = $label;
+			console.log($item)
+		};
+		$scope.onSelect3 = function ($item, $model, $label) {
+			$scope.$item3 = $item;
+			$scope.$model3 = $model;
+			$scope.$label3 = $label;
+			console.log($item)
+		};
+		$scope.onSelect5 = function ($item, $model, $label) {
+			$scope.$item5 = $item;
+			$scope.$model5 = $model;
+			$scope.$label5 = $label;
+			console.log($item)
+		};
 }
