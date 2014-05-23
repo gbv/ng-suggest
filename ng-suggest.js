9730443fc9cd5d180706efaa44b8a7596be9a857
Moritz Horn <moritz.horn@gmail.com>
2014-05-23 13:25:27 +0200

    added htmlescape filter for the template

--- ng-suggest.js
+++ ng-suggest.js
@@ -216,6 +216,11 @@ angular.module('ngSuggest').directive('suggestTypeahead', [
     };
   }
 ]);
+angular.module('ngSuggest').filter('htmlescape', function () {
+  return function (input) {
+    return input ? input.replace(/[<>]/g, '-') : '';
+  };
+});
 /**
  * @ngdoc service
  * @name ng-suggest.service:OpenSearchSuggestions
@@ -493,6 +498,6 @@ angular.module('ngSuggest').run([
   function ($templateCache) {
     'use strict';
     $templateCache.put('template/seealso-response.html', '<ul ng-if="links"><li ng-repeat="link in links"><a ng-if="link.url" href="{{link.url}}" title="{{link.description}}">{{link.label}}</a> <span ng-if="!link.url">{{link.label}}</span></li></ul>');
-    $templateCache.put('template/suggest-typeahead.html', '<a tabindex="-1" class="suggest-typeahead-item"><div bind-html-unsafe="match.label | typeaheadHighlight:query" class="suggest-typeahead-label"></div><i ng-if="match.model.description" class="suggest-typeahead-description">{{match.model.description}}</i></a>');
+    $templateCache.put('template/suggest-typeahead.html', '<a tabindex="-1" class="suggest-typeahead-item"><div bind-html-unsafe="match.label | htmlescape |  typeaheadHighlight:query" class="suggest-typeahead-label"></div><i ng-if="match.model.description" class="suggest-typeahead-description">{{match.model.description}}</i></a>');
   }
 ]);
